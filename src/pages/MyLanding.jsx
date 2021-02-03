0aaa4bfe5499d271b982e861b0e8f0cc4530734a
riyaraa <rinosatyaputra.id@gmail.com>
2021-02-03 22:50:30 +0700

    assets profile

--- src/pages/MyLanding.jsx
+++ src/pages/MyLanding.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Headers from "../components/parts/Header";
+import Header from "../components/parts/Header";
 import Hero from "../components/parts/Hero";
 import Footer from "../components/parts/Footer";
 export default function MyLanding() {
 	return (
 		<>
-			<Headers />
+			<Header />
 			<Hero />
 			<Footer />
 		</>
